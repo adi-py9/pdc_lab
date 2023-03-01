e45d708f182b17bc0441a089ff7dcb2016b8aa12
adi-py9 <aditya.py9@gmail.com>
2023-03-01 11:23:37 +0530

    lab3 q2 done q1 o/p grabage value

--- lab3/q1_20bce0456.c
+++ lab3/q1_20bce0456.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include <omp.h>
 
-#define N 1000
+#define N 100
+#define M 100
 
 int main() {
-    int a[N][N], b[N][N], c[N][N];
+    int a[N][M], b[N][M], c[N][M];
     int i, j, k;
 
     // initialize matrices
     for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
+        for (j = 0; j < M; j++) {
             a[i][j] = i + j;
             b[i][j] = i - j;
-            c[i][j] = 0;
+           // c[i][j] = 0;
         }
     }
+    //test : printing a matrix
+     for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    //test : printing b matrix
+     for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+
 
     // perform matrix multiplication using OpenMP parallelization
     #pragma omp parallel for shared(a,b,c) private(i,j,k)
     for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
+        for (j = 0; j < M; j++) {
             for (k = 0; k < N; k++) {
                 c[i][j] += a[i][k] * b[k][j];
             }
@@ -27,12 +44,12 @@ int main() {
     }
 
     // print result matrix
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            printf("%d ", c[i][j]);
-        }
-        printf("\n");
-    }
+    // for (i = 0; i < N; i++) {
+    //     for (j = 0; j < M; j++) {
+    //         printf("%d ", c[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
     return 0;
 }

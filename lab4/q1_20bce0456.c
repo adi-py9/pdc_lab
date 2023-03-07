df1c20ef7d12084b2bcd2e23a63b63547d0baa98
adi-py9 <aditya.py9@gmail.com>
2023-03-07 23:26:41 +0530

    pdc lab4 codes updated

--- lab4/q1_20bce0456.c
+++ lab4/q1_20bce0456.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv) {
     int rank, size;
+    // Initialize MPI environment
+    MPI_Init(&argc, &argv); 
     
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    
-    printf("Hello World from process %d of %d!\n", rank, size);
-    
-    MPI_Finalize();
-    
+    // fxn gets the rank of the current process
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+    // fxn gets the total number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+
+    //printing the results
+    printf("\nHello, world! I am process %d of %d.\n", rank, size);
+    MPI_Finalize(); // to finalize MPI environment
     return 0;
 }
--- lab4/q2_20bce0456.c
+++ lab4/q2_20bce0456.c
@@ -6,15 +6,18 @@ int main(int argc, char *argv[]) {
     int rank, size;
     char name[MPI_MAX_PROCESSOR_NAME];
     int len;
-    
+
+    // Initialize MPI environment
     MPI_Init(&argc, &argv);
+
+    // fxn gets the rank of the current process
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // fxn gets the total number of processes
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     MPI_Get_processor_name(name, &len);
     
-    printf("Hello from processor %d of %d on %s\n", rank, size, name);
-    
-    MPI_Finalize();
-    
+    printf("\nHello from processor %d of %d on %s\n", rank, size, name);
+    MPI_Finalize();// to finalize MPI environment
     return 0;
 }
